97742ebe7eeea42f50755117859c4f29064e444d
Mac Mini <macmini@Mac-mini-de-Mac.local>
2024-12-27 12:57:30 +0100

    addtaskview, task, taskviewmodel

---
+++ Shared/EditTaskView.swift
@@ -0,0 +1,8 @@
+//
+//  EditTaskView.swift
+//  PracticaM08
+//
+//  Created by Mac Mini on 27/12/24.
+//
+
+import Foundation
---
+++ Shared/ListItem.swift
@@ -0,0 +1,8 @@
+//
+//  ListItem.swift
+//  PracticaM08
+//
+//  Created by Mac Mini on 27/12/24.
+//
+
+import Foundation
